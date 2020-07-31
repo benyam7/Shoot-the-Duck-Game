dc33bef67c00f15bd27ee15658237c9e7045a775
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2020-07-31 12:58:21 +0300

    minor refactors

--- 2D Duck Simulation/src/ImageLoader.java
+++ 2D Duck Simulation/src/ImageLoader.java
@@ -17,6 +17,7 @@ public class ImageLoader {
 	public  BufferedImage loadImage(String path)throws IOException
 	{
 		image = ImageIO.read(getClass().getResource(path));
+
 		return image;
 	}
 }
---
+++ 2D Duck Simulation/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Resources used: Google and tutorial
+ * 
+ * */
+
+public class Main {
+
+	public static void main(String[] args){
+
+		GameCore game = new GameCore("Duck Simulation Game :)", 640, 480);
+
+		game.start();
+
+		
+		
+	}
+}
